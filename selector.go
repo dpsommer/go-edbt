d60bbe94641d784cf9bbc217529723a178500eab
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-01-15 00:42:36 -0500

    Fix sequencer and selector iteration
    * Previously, selectors and sequencers would tick over each
      child on each received tick; this implementation meant that
      if a Running state was returned from an nth child, future
      ticks could return erroneous states by re-ticking a child
      that was already checked
    * This change modifies the behaviour of selectors and sequencers
      by including a call to Children in the Initialize function
    * Children now returns an iterator seq and a closure next that
      allow the caller to hold positional iteration and resume on
      subsequent ticks if a Running state is returned
    * If a non-Running state is returned, Initialize will be called,
      resetting the iterator
    * Each instantiation of the iterator creates a copy of the current
      set of children to avoid issues upon future modification

--- selector.go
+++ selector.go
@@ -1,10 +1,17 @@
 package goedbt
 
+import (
+	"iter"
+)
+
 // Selector defines a Behaviour BehaviourNode that checks each of its children,
 // returning the first non-Failure status or Failure if all children fail
 type Selector struct {
 	*node
 	*composite
+
+	seq  iter.Seq[Behaviour]
+	next func()
 }
 
 func NewSelector() *Selector {
@@ -16,17 +23,22 @@ func NewSelector() *Selector {
 	}
 }
 
-func (n *Selector) Initialize() {}
-func (n *Selector) Terminate()  {}
+func (n *Selector) Initialize() {
+	n.seq, n.next = n.Children()
+}
 
 func (n *Selector) Update() Status {
-	for c := range n.children {
-		if status := tick(c); status != Failure {
+	for c := range n.seq {
+		status := tick(c)
+		if status != Failure {
 			n.state = status
 			return n.state
 		}
+		n.next()
 	}
 
 	n.state = Failure
 	return n.state
 }
+
+func (n *Selector) Terminate() {}
--- sequencer_test.go
+++ sequencer_test.go
@@ -6,19 +6,51 @@ import (
 	goedbt "github.com/dpsommer/go-edbt"
 )
 
-func TestSequencer(t *testing.T) {
-	sequencer := goedbt.NewSequencer()
-	sequencer.AddChild(&goedbt.SuccessBehaviour{})
-	tree := goedbt.NewBehaviourTree(sequencer)
-
-	status := goedbt.Tick(tree.Root)
-	if status != goedbt.Success {
-		t.Errorf("Sequencer got %d, want %d", status, goedbt.Success)
+func setupCompositeTree(c goedbt.Composite, tasks ...goedbt.Behaviour) *goedbt.BehaviourTree {
+	for _, t := range tasks {
+		c.AddChild(t)
 	}
 
-	sequencer.AddChild(&goedbt.FailureBehaviour{})
-	status = goedbt.Tick(tree.Root)
-	if status != goedbt.Failure {
-		t.Errorf("Sequencer got %d, want %d", status, goedbt.Failure)
+	return goedbt.NewBehaviourTree(c)
+}
+
+func TestSequencer(t *testing.T) {
+	tt := map[string]struct {
+		behaviours []goedbt.Behaviour
+		expected   []goedbt.Status
+	}{
+		"returns success then failure": {
+			behaviours: []goedbt.Behaviour{
+				&goedbt.XThenY{X: goedbt.Success, Y: goedbt.Failure},
+			},
+			expected: []goedbt.Status{goedbt.Success, goedbt.Failure},
+		},
+		"returns running": {
+			behaviours: []goedbt.Behaviour{
+				&goedbt.SuccessBehaviour{},
+				&goedbt.RunningBehaviour{},
+			},
+			expected: []goedbt.Status{goedbt.Running},
+		},
+		"returns running then success": {
+			behaviours: []goedbt.Behaviour{
+				&goedbt.SuccessBehaviour{},
+				&goedbt.XThenY{X: goedbt.Running, Y: goedbt.Success},
+			},
+			expected: []goedbt.Status{goedbt.Running, goedbt.Success},
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tree := setupCompositeTree(goedbt.NewSequencer(), tc.behaviours...)
+
+			for _, s := range tc.expected {
+				status := goedbt.Tick(tree.Root)
+				if status != s {
+					t.Errorf("Selector got %d, want %d", status, s)
+				}
+			}
+		})
 	}
 }
