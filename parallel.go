9553d5b1982ecb2efcfdf060be4f76f88bdea440
Duncan Sommerville <duncan.sommerville@gmail.com>
2025-01-15 03:43:24 -0500

    Reimplement Parallel composite node
    * Abort any still-running children on teardown
    * Remove unecessary concurrency
    * Add successPolicy configuration

--- parallel.go
+++ parallel.go
@@ -1,7 +1,10 @@
 package goedbt
 
-import (
-	"context"
+type Policy int
+
+const (
+	RequireOne Policy = iota
+	RequireAll
 )
 
 // Parallel defines a composite Behaviour that ticks its children
@@ -9,46 +12,77 @@ import (
 // If all children fail, returns Failure.
 type Parallel struct {
 	*composite
+
+	// only specify a successPolicy - this implicitly defines the failure
+	// policy and avoids ambiguity (e.g. if successPolicy and failurePolicy
+	// are both RequireAll)
+	successPolicy Policy
+
+	runningNodes []Behaviour
 }
 
-func NewParallel() *Parallel {
+func NewParallel(successPolicy Policy) *Parallel {
 	return &Parallel{
+		successPolicy: successPolicy,
 		composite: &composite{
 			node:     &node{state: Invalid},
-			children: make(map[Behaviour]struct{}),
+			children: make(Set[Behaviour]),
 		},
 	}
 }
 
-func (n *Parallel) Initialize() {}
-func (n *Parallel) Terminate()  {}
+func (n *Parallel) Initialize() {
+	n.runningNodes = keys(n.children)
+}
 
 func (n *Parallel) Update() Status {
+	var b Behaviour
+	// track success count and required number of successes for RequireAll
+	// we only need len(runningNodes) successes each tick as we can imply that:
+	// * if RequireOne, we haven't succeeded yet but any Success will be enough
+	// * if RequireAll, completed tasks must have been Success as we fail fast
+	var successes int
+	needSuccesses := len(n.runningNodes)
+
+	// default to Failure so we don't need to count Failures for RequireOne
 	status := Failure
-	results := make(chan Status, 1)
+	// list all still-running tasks; check only that subset on subsequent ticks
+	stillRunning := make([]Behaviour, needSuccesses)
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	for len(n.runningNodes) > 0 {
+		b, n.runningNodes = pop(n.runningNodes)
+		res := tick(b)
 
-	for c := range n.children {
-		go func(ctx context.Context, c Behaviour) {
-			select {
-			case results <- tick(c):
-			case <-ctx.Done():
-			}
-		}(ctx, c)
-	}
-
-	for range len(n.children) {
-		res := <-results
 		if res == Success {
-			n.state = Success
-			return n.state
+			successes += 1
+			if n.successPolicy == RequireOne || successes == needSuccesses {
+				n.state = Success
+				return n.state
+			}
 		} else if res == Running {
 			status = Running
+			stillRunning = append(stillRunning, b)
+		} else if n.successPolicy == RequireAll {
+			// fail fast if a child task failed or was aborted
+			n.state = res
+			return n.state
 		}
+	}
+
+	if status == Running {
+		n.runningNodes = stillRunning
 	}
 
 	n.state = status
 	return n.state
 }
+
+func (n *Parallel) Teardown() {
+	// if this node is configured to Teardown early,
+	// abort any running children
+	for c := range n.children {
+		c.Abort()
+	}
+}
+
+func (n *Parallel) Abort() {}
